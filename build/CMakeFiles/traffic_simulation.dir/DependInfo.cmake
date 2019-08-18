fda9e41a58069cf2ff71727ce8c7aaea4655314e
Bander Alshammari <bannadr1@hotmail.com>
2019-08-19 01:56:19 +0300

    creat a thread for a function of an instance

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bander/Development/cpp/cpp_concurrency_L2_project/src/Graphics.cpp" "/Users/bander/Development/cpp/cpp_concurrency_L2_project/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/Users/bander/Development/cpp/cpp_concurrency_L2_project/src/Intersection.cpp" "/Users/bander/Development/cpp/cpp_concurrency_L2_project/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/Users/bander/Development/cpp/cpp_concurrency_L2_project/src/Street.cpp" "/Users/bander/Development/cpp/cpp_concurrency_L2_project/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/Users/bander/Development/cpp/cpp_concurrency_L2_project/src/TrafficObject.cpp" "/Users/bander/Development/cpp/cpp_concurrency_L2_project/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/Users/bander/Development/cpp/cpp_concurrency_L2_project/src/TrafficSimulator-L2.cpp" "/Users/bander/Development/cpp/cpp_concurrency_L2_project/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L2.cpp.o"
+  "/Users/bander/Development/cpp/cpp_concurrency_L2_project/src/Vehicle.cpp" "/Users/bander/Development/cpp/cpp_concurrency_L2_project/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
